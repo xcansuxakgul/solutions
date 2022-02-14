b52ddd6f1383fd58df9f22936801b7efb17e7a74
Emine Cansu Akgül <eminecansuakgul@gmail.com>
2022-02-14 15:25:45 +0300

    Add files via upload

---
+++ lexicographicalorder.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'biggerIsGreater' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING w as parameter.
+#
+
+def biggerIsGreater(w):
+    # Write your code here
+    n = len(w)
+
+    for i in reversed(range(n-1)):
+
+        for j in reversed(range(i+1,n)):
+
+            if w[j] > w[i]:
+
+                return w[:i] + w[j] + ''.join(sorted(w[i:j] + w[j+1:]))
+
+    return 'no answer'
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    T = int(input().strip())
+
+    for T_itr in range(T):
+        w = input()
+
+        result = biggerIsGreater(w)
+
+        fptr.write(result + '\n')
+
+    fptr.close()
+
+
